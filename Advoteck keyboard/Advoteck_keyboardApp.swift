2a18ab95e753b1726598e39637a61036cc7c66a0
Pavneet Cheema <Pavneetsingh685@gmail.com>
2024-02-17 17:59:36 -0600

    Initial Commit

---
+++ Advoteck keyboard/Advoteck_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advoteck_keyboardApp.swift
+//  Advoteck keyboard
+//
+//  Created by Pavneet Cheema on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Advoteck_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
